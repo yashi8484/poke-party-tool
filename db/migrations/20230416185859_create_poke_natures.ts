da6de6af7c732bf2ecda11dcea5d394181e5cae4
yashi8484 <abok@yashi84.com>
2023-04-16 19:42:40 +0900

    せいかくテーブル作成用マイグレーションファイルを追加する

---
+++ db/migrations/20230416185859_create_poke_natures.ts
@@ -0,0 +1,24 @@
+import {
+  AbstractMigration,
+  Info,
+  ClientPostgreSQL,
+} from "https://deno.land/x/nessie@2.0.10/mod.ts";
+
+export default class extends AbstractMigration<ClientPostgreSQL> {
+  /** Runs on migrate */
+  async up(info: Info): Promise<void> {
+    await this.client.queryArray(`
+      CREATE TABLE IF NOT EXISTS public.poke_natures (
+        id serial primary key,
+        name varchar(20) NOT NULL UNIQUE,
+        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+      );
+    `);
+  }
+
+  /** Runs on rollback */
+  async down(info: Info): Promise<void> {
+    await this.client.queryArray(`DROP TABLE IF EXISTS public.poke_natures;`);
+  }
+}
