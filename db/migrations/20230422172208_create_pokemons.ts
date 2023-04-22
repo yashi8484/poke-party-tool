5428e46b166ee316461dd2a324487527d214e5e2
yashi8484 <abok@yashi84.com>
2023-04-22 17:29:11 +0900

    ポケモンデータ格納用テーブルを追加する

---
+++ db/migrations/20230422172208_create_pokemons.ts
@@ -0,0 +1,26 @@
+import {
+  AbstractMigration,
+  Info,
+  ClientPostgreSQL,
+} from "https://deno.land/x/nessie@2.0.10/mod.ts";
+
+export default class extends AbstractMigration<ClientPostgreSQL> {
+  /** Runs on migrate */
+  async up(info: Info): Promise<void> {
+    await this.client.queryArray(`
+        CREATE TABLE IF NOT EXISTS public.pokemons (
+          id serial primary key,
+          pokedex_number smallint NOT NULL,
+          species_id smallint NOT NULL UNIQUE,
+          name varchar(20) NOT NULL,
+          created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+          updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+        );
+      `);
+  }
+
+  /** Runs on rollback */
+  async down(info: Info): Promise<void> {
+    await this.client.queryArray(`DROP TABLE IF EXISTS public.pokemons;`);
+  }
+}
